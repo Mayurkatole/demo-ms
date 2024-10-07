3304dbc9be8c253c653914cd15de6cede17152f9
mayurkatole <mayurkatole2@gmail.com>
2024-10-07 17:12:30 +0530

    coderefactor

---
+++ src/test/java/com/example/demo_ms/DemoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
